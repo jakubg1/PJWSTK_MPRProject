55a7482e29afec7e3550cea491e82cebf5cc2010
jakubg1 <24206305+jakubg1@users.noreply.github.com>
2025-01-25 15:47:22 +0100

    Initial commit

---
+++ src/main/java/com/pjatk/s32362Bank/S32362BankApplication.java
@@ -0,0 +1,13 @@
+package com.pjatk.s32362Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S32362BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S32362BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pjatk/s32362Bank/S32362BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjatk.s32362Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S32362BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
